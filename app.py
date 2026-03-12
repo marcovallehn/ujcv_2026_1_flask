c0fd86e558924743d6b6d6da23ebe220af81417a
Marco Valle <marcovallehn@gmail.com>
2026-03-12 08:28:19 -0600

    clase del 12 de marzo 2026

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ conn = mysql.connector.connect(
         )
 
 
-@app.route("/")
-def index():
+@app.route("/clientes/")
+def clientes_index():
     cur = conn.cursor()
     cur.execute("SELECT * FROM clientes")
     datos = cur.fetchall()
     cur.close()
-    return render_template('index.html', lista_clientes=datos)
+    return render_template('clientes/index.html', lista_clientes=datos)
 
 
-@app.route("/agregar", methods=["GET", "POST"])
-def agregar():
+@app.route("/clientes/agregar", methods=["GET", "POST"])
+def agregar_datos():
     if request.method == 'POST':
         cursor = conn.cursor()
         nombre= request.form['CliNombre']
@@ -32,20 +32,18 @@ def agregar():
         direccion=request.form['CliDireccion']
         cursor.execute("insert into clientes(CliNombre,CliApellido, CliCorreo, CliTelefono, CliDireccion) values(%s, %s, %s, %s, %s)", (nombre, apellido, correo, telefono, direccion))
         conn.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('clientes_index'))
     elif request.method=='GET':
-         return render_template('agregar.html')
+         return render_template('/clientes/agregar.html')
 
 
-
-
-@app.route("/editar/<string:codigo>",  methods=["GET","POST"])
+@app.route("/clientes/editar/<string:codigo>",  methods=["GET","POST"])
 def editar(codigo):
     if request.method == 'GET':
         cur = conn.cursor()
         cur.execute("SELECT * FROM clientes where CliCodigo=%s", (codigo,))
         cliente = cur.fetchone()
-        return render_template('editar.html', cliente=cliente)
+        return render_template('/clientes/editar.html', cliente=cliente)
     elif request.method=='POST': 
         cursor = conn.cursor()
         nombre= request.form['CliNombre']
@@ -55,12 +53,34 @@ def editar(codigo):
         direccion=request.form['CliDireccion']
         cursor.execute("update clientes set CliNombre=%s , CliApellido=%s, CliCorreo=%s, CliTelefono=%s, CliDireccion=%s where CliCodigo=%s", (nombre, apellido, correo, telefono, direccion,codigo))
         conn.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('clientes_index'))
 
 
-@app.route("/eliminar")
-def eliminar():
-    pass
+@app.route("/clientes/eliminar/<string:codigo>",  methods=["GET","POST"])
+def eliminar(codigo):
+    if request.method == 'GET':
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM clientes where CliCodigo=%s", (codigo,))
+        cliente = cursor.fetchone()
+        return render_template('/clientes/eliminar.html', cliente=cliente)
+    elif request.method=='POST': 
+        cursor = conn.cursor()
+        cursor.execute(" delete from clientes where CliCodigo=%s", (codigo,))
+        conn.commit()
+        return redirect(url_for('clientes_index'))
+
+
+@app.route("/productos/")
+def productos_index():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM productos")
+    datos = cur.fetchall()
+    cur.close()
+    return render_template('productos/index.html', lista_productos=datos)
+
+
+
+
 
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
