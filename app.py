f1cbccbca1b005d296abf9673d21ac177856c35a
Marco Nalle <marcovallehn@gmail.com>
2026-03-17 08:21:27 -0600

    clase 17 de marzo

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ conn = mysql.connector.connect(
             host='localhost',
             user='root',
             password='',
-            database='ujcv_2026_1_progra2',
+            database='ujcv_2026_2_progra2',
             ssl_disabled=True  # Desactiva SSL
         )
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 
 
 @app.route("/clientes/")
@@ -77,6 +83,43 @@ def productos_index():
     datos = cur.fetchall()
     cur.close()
     return render_template('productos/index.html', lista_productos=datos)
+
+
+
+@app.route("/productos/agregar", methods=["GET", "POST"])
+def productos_agregar_datos():
+    if request.method == 'POST':
+        cursor = conn.cursor()
+        nombre= request.form['ProNombre']
+        descripcion = request.form['ProDescripcion']
+        precio = request.form['ProPrecio']
+        costo = request.form['ProCosto']
+        stock=request.form['ProStock']
+        cursor.execute("insert into productos(ProNombre, ProDescripcion, ProPrecio, ProCosto, ProStock) values(%s, %s, %s, %s, %s)", (nombre, descripcion, precio, costo, stock))
+        conn.commit()
+        return redirect(url_for('productos_index'))
+    elif request.method=='GET':
+         return render_template('/productos/agregar.html')
+
+
+@app.route("/productos/editar/<string:codigo>", methods=["GET", "POST"])
+def productos_editar_datos(codigo):
+    if request.method == 'GET':
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM productos where ProCodigo=%s", (codigo,))
+        producto = cur.fetchone()
+        return render_template('/productos/editar.html', producto=producto)
+    elif request.method=='POST':
+        cursor = conn.cursor()
+        nombre= request.form['ProNombre']
+        descripcion = request.form['ProDescripcion']
+        precio = request.form['ProPrecio']
+        costo = request.form['ProCosto']
+        stock=request.form['ProStock']
+        cursor.execute("update productos set ProNombre=%s, ProDescripcion=%s, ProPrecio=%s, ProCosto=%s, ProStock=%s where ProCodigo=%s", (nombre, descripcion, precio, costo, stock, codigo))
+        conn.commit()
+        return redirect(url_for('productos_index'))
+    
 
 
 
